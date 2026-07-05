bd463fbdc20c8645dee0d019b8e2110aeac0ee26
madhuradhawangale00-sys <madhuradhawangale00-sys@email.com>
2026-07-05 15:39:22 +0530

    Implement register route with email availability check

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,8 +1,31 @@
+const User = require("../models/User");
 const registerUser = async (req,res) => {
-    res.json({
-        success: true,
-        message: "Register Controller Working",
-    });
+    try {
+        const { name, email, password } = req.body;
+
+        const existingUser = await User.findOne({email});
+
+        if(existingUser) {
+            return res.status(400).json({
+                success: false,
+                message: "User already exists",
+            });
+        }
+
+        console.log(req.body);
+
+        res.status(200).json({
+         success: true,
+          message: "Email is available",
+           });
+        
+
+    }catch(error) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+        });
+    }
 };
 
 module.exports = { registerUser};
