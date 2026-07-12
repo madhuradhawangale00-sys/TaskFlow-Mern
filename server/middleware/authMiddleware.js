a82b13bbab7ff04e98fb4c9f4d05930e1997920e
madhuradhawangale00-sys <madhuradhawangale00-sys@email.com>
2026-07-12 11:00:34 +0530

    token extracted

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,9 +1,20 @@
 const jwt = require("jsonwebtoken");
 const protect = (req,res,next) => {
-    console.log(req.headers);
+
+    let token;
+
+    if(
+        req.headers.authorization &&
+        req.headers.authorization.startsWith("Bearer")
+    ){
+        token = req.headers.authorization.split(" ")[1];
+
+        console.log(token);
+        
+    }
 
     next();
     
-}
+};
 
 module.exports = protect;
