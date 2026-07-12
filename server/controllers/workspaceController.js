969adfbe4787f69dbef5086877802eb70dd9b449
madhuradhawangale00-sys <madhuradhawangale00@gmail.com>
2026-07-12 23:36:19 +0530

    stored first workspace in MongoDb

--- server/controllers/workspaceController.js
+++ server/controllers/workspaceController.js
@@ -1,7 +1,26 @@
+const Workspace = require("../models/Workspace");
 const createWorkspace = async (req, res) => {
-    res.json({
-        message: "Worksoace Controller Working",
-    });
-};
+    try{
+        const { name, description} = req.body;
+
+        const workspace = await Workspace.create({
+            name,
+            description,
+            owner: req.user._id,
+            members: [ req.user._id]
+        });
+        res.status(201).json({
+            success: true,
+            message: "Workspace created successfully",
+            workspace
+        });
+
+    }catch(error){
+        res.status(500).json({
+            success:true,
+            message: error.message
+        });
+    }
+}
 
 module.exports = {createWorkspace};
